7bf8c0b34dc0764d53f8a0ca9c7a14c7850801bf
shellqiqi <shellqiqi@outlook.com>
2019-11-06 19:13:43 +0800

    rename seu to nju

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
-xelatex SEU_BeamerTemplate
-xelatex SEU_BeamerTemplate
+xelatex NJU_BeamerTemplate
+xelatex NJU_BeamerTemplate
 rm -f *.aux *.bak *.log *.bbl *.dvi *.blg *.thm *.toc *.out *.lof *.lol *.lot *.nav *.snm *.fdb_latexmk *.synctex.gz
